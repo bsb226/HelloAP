4989b493f1217ccb91bc3e563fe3ce3b4fda1277
Brian Borowski <bsb2151@columbia.edu>
2026-05-28 17:18:45 -0400

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 echo "Hello, AP!"
-echo "I love this course!"
-echo "I'm not so sure about Dr. B -- culpa"
-echo "I also like 3136."
-echo "I really hate AP!"
-echo "Is this thing on?"
